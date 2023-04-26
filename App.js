cfa0d64620f63dd4034445448702047438df9e15
31000s <burakyabgu@gmail.com>
2023-04-26 04:42:14 +0300

    fix:  'assets' deleted

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
